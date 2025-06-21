c340fb6d8d4e73959974d6c5ba2b41a0a2ffec2a
Ganendra Wibowo <chesnaganendra.2023@student.uny.ac.id>
2025-06-21 19:24:58 +0700

    kelar modul 15

--- bullet.gd
+++ bullet.gd
@@ -7,3 +7,9 @@ func _process(delta):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Enemy"):
+		body.visible = false
+		hide()
